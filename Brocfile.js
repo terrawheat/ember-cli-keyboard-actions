6c78547458b08c0cfabeaf62707452653742ccc3
Matt Wheatley <terrawheat@gmail.com>
2015-05-04 21:03:04 +0100

    Installed ember-mocha

--- Brocfile.js
+++ Brocfile.js
@@ -2,8 +2,13 @@
 /* global require, module */
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-
-var app = new EmberAddon();
+var path = require('path');
+var jsStringEscape = require('js-string-escape');
+var app = new EmberAddon({
+  eslint: {
+    testGenerator: eslintTestGenerator
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -18,4 +23,22 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+function render(errors) {
+  if (!errors) { return ''; };
+  return errors.map(function(error) {
+    return error.line + ':' + error.column + ' ' +
+      ' - ' + error.message + ' (' + error.ruleId +')';
+  }).join('\n');
+}
+
+function eslintTestGenerator(relativePath, errors) {
+  var pass = !errors || errors.length === 0;
+  return "import { describe, it }  from 'mocha';\n" +
+    "import { assert } from 'chai';\n" +
+    "describe('ESLint - " + path.dirname(relativePath) + "', function() {\n" +
+    "  it('" + relativePath + " should pass ESLint', function() {\n" +
+    "    assert.ok(" + pass + ", '" + relativePath + " should pass ESLint." +
+    jsStringEscape("\n" + render(errors)) + "');\n" +
+    "  });\n});\n";
+}
 module.exports = app.toTree();
